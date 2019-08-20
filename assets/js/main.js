c9846f08cf24e43ccf1f64078718273db8d7a99b
justinChen <yub612@gmail.com>
2019-08-21 00:55:00 +0800

    add auto lesson generator

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){ 
+    fetch("/courses").then(function(response) {
+        return response.json();
+    }).catch(function(err){
+        throw err;
+    }).then(function(json){
+        // console.log(json);
+        json.courses.forEach(element => {
+            console.log(element)
+            $("#lesson_lists").append(
+                `<div class="lesson-card text-left">
+                    <i class="mdi mdi-flag-outline icon text-center text-info"></i>
+                    <div class="lesson-card-text">
+                        <h5>${element.title}</h5>
+                        <p>上課時間: ${element.time}</p>
+                    </div>
+                    
+                    <button type="button" class="btn btn-outline-info lesson-btn" data-toggle="modal" data-target="#lesson${element.id}">了解更多</button>
+        
+                    <!-- modal -->
+                    <div class="modal fade" id="lesson${element.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title text-dark" id="exampleModalCenterTitle">${element.title}</h5>
+                            </div>
+                            <div class="modal-body">
+                                <img src="${element.img}" class="modal-img">
+                                <p class="text-dark pt-3">${element.description}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">關 閉</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                </div>`
+            )
+        });
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    console.log("main page");
+router.get('/courses', function(req, res, next) {
+    var jsonPath = path.resolve(__dirname,"../courses.json");
+    var course = JSON.parse(fs.readFileSync(jsonPath,'utf-8'));
+    res.json(course)
+    console.log(`${JSON.stringify(course)}`)
 });
 
 module.exports = router;
